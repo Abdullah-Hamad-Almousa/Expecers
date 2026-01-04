6f4905c8c181664821ded15abbad36f78a4f4202
Abdullah almousa <abdullahmlwork@gmail.com>
2026-01-05 01:10:33 +0300

    add expect for android and common

---
+++ composeApp/src/androidMain/kotlin/com/mysticbyte/expecers/exp/DeviceInfo.android.kt
@@ -0,0 +1,10 @@
+package com.mysticbyte.expecers.exp
+
+actual class DeviceInfo {
+    actual val platform: String
+        get() = TODO("Not yet implemented")
+    actual val model: String
+        get() = TODO("Not yet implemented")
+    actual val osVersion: String
+        get() = TODO("Not yet implemented")
+}
---
+++ composeApp/src/commonMain/kotlin/com/mysticbyte/expecers/exp/DeviceInfo.kt
@@ -0,0 +1,9 @@
+package com.mysticbyte.expecers.exp
+
+expect class DeviceInfo {
+
+    val platform: String
+    val model: String
+    val osVersion: String
+
+}
